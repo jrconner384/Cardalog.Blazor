cbb675828dd436b61e8c61c35dee0bc348a5adf2
Jason Conner <jason.r.conner@gmail.com>
2019-11-23 21:24:29 -0600

    Adding a basic card page and some dummy data.

---
+++ Models/Card.cs
@@ -0,0 +1,13 @@
+public class Card
+{
+  public string Title { get; set; }
+  public string Expansion { get; set; }
+  public string Description { get; set; }
+  public string ColorText { get; set; }
+  public int CostBlack { get; set; }
+  public int CostBlue { get; set; }
+  public int CostColorless { get; set; }
+  public int CostGreen { get; set; }
+  public int CostRed { get; set; }
+  public int CostWhite { get; set; }
+}
