2520afd45784c295eeefaf7558b14f364bc3b9b6
Jason Conner <jason.r.conner@gmail.com>
2019-12-05 14:07:09 -0600

    Improving cost tracking based on recent API changes. Trying some JS interop to reset the form after submitting it.

--- Models/Card.cs
+++ Models/Card.cs
@@ -18,12 +18,14 @@ namespace Cardalog.Models
     [StringLength(25, ErrorMessage = "{0} may not exceed {1] characters.")]
     public string Rarity { get; set; }
 
-    [StringLength(15, ErrorMessage = "{0} may not exceed {1] characters.")]
-    public string Cost { get; set; }
+    public Cost Cost { get; set; }
 
-    [Required]
-    [Range(0, 20, ErrorMessage = "{0} must be between {1} and {2}.")]
-    public int? ConvertedCost { get; set; }
+    // [StringLength(15, ErrorMessage = "{0} may not exceed {1] characters.")]
+    // public string Cost { get; set; }
+
+    // [Required]
+    // [Range(0, 20, ErrorMessage = "{0} must be between {1} and {2}.")]
+    // public int? ConvertedCost { get; set; }
 
     [StringLength(250, ErrorMessage = "{0} may not exceed {1] characters.")]
     public string Text { get; set; }
@@ -61,5 +63,39 @@ namespace Cardalog.Models
 
     [StringLength(250, ErrorMessage = "{0} may not exceed {1] characters.")]
     public string Copyright { get; set; }
+  }
+
+  public class Cost
+  {
+    [Range(0, 15, ErrorMessage = "{0} must be between {1} and {2}.")]
+    public int? Black { get; set; }
+
+    [Range(0, 15, ErrorMessage = "{0} must be between {1} and {2}.")]
+    public int? Blue { get; set; }
+
+    [Range(0, 15, ErrorMessage = "{0} must be between {1} and {2}.")]
+    public int? Colorless { get; set; }
+
+    [Range(0, 90, ErrorMessage = "{0} must be between {1} and {2}.")]
+    public int? Converted 
+    {
+      get
+      {
+        return Blue + Black + Colorless + Green + Red + White;
+      }
+      set
+      {
+        
+      }
+    }
+
+    [Range(0, 15, ErrorMessage = "{0} must be between {1} and {2}.")]
+    public int? Green { get; set; }
+
+    [Range(0, 15, ErrorMessage = "{0} must be between {1} and {2}.")]
+    public int? Red { get; set; }
+
+    [Range(0, 15, ErrorMessage = "{0} must be between {1} and {2}.")]
+    public int? White { get; set; }
   }
 }
