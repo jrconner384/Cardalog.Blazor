1eb52011044196c7c311a27e1d53b8d956faa6cd
Jason Conner <jason.r.conner@gmail.com>
2019-12-10 17:28:20 -0600

    Tearing out BlazorStrap the rest of the way. Adding in Bootstrap, JQuery, and Popper.js references per Bootstrap's instructions.

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
-using BlazorStrap;
 
 namespace Cardalog
 {
@@ -8,7 +7,6 @@ namespace Cardalog
     {
         public void ConfigureServices(IServiceCollection services)
         {
-          services.AddBootstrapCSS();
         }
 
         public void Configure(IComponentsApplicationBuilder app)
